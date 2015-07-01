d32e465c87b44674abfd17f142f2ba727798cdc3
Julien Balestra <julien.balestra@gmail.com>
2015-07-01 20:39:39 +0200

    ASM format header

--- libftASM/includes/libftasm.h
+++ libftASM/includes/libftasm.h
@@ -3,26 +3,31 @@
 # include <strings.h>
 # include <stdlib.h>
 
-
+/* part one */
+void ft_bzero(void *s, size_t n);
+char *ft_strcat(char *dest, const char *src);
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
-int	ft_isblank(int c);
 int	ft_isprint(int c);
-int	ft_islower(int c);
-int	ft_isupper(int c);
 int	ft_toupper(int c);
 int	ft_tolower(int c);
-
-size_t ft_strlen(char *str);
-void ft_bzero(void *s, size_t n);
 int ft_puts(const char *s);
+
+/* part two */
+size_t ft_strlen(char *str);
 void *memset(void *s, int c, size_t n);
-void *ft_memalloc(size_t size);
-char *ft_strcat(char *dest, const char *src);
 void *memcpy(void *dest, const void *src, size_t n);
 char *ft_strdup(const char *s);
+
+/* Bonus */
 void ft_cat(int fd);
+
+/* Extra */
+int	ft_isblank(int c);
+int	ft_islower(int c);
+int	ft_isupper(int c);
+void *ft_memalloc(size_t size);
 
 #endif
