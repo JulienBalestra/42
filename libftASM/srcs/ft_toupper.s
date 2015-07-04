148b85d4150f019626a61de4f691ab2cd985622e
Julien Balestra <julien.balestra@gmail.com>
2015-07-04 17:52:49 +0200

    To Lower with ABS

--- libftASM/srcs/ft_toupper.s
+++ libftASM/srcs/ft_toupper.s
@@ -1,14 +1,21 @@
+extern ft_abs
 
 section	.text
 	global ft_toupper
 
 ft_toupper:
 	enter	0, 0	; save register
+	call ft_abs
+	mov rdi, rax
 	cmp	rdi, 122
 	jg	fail
 	cmp	rdi, 97
 	jl	fail
 	jmp	success
+
+negative:
+	mov rdi, 1
+	jmp fail
 
 fail:
 	mov	rax, rdi
